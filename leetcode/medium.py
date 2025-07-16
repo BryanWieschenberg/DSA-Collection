366b411e7abcbd3d1d3beeec5e8955e809e14e55
BryanWieschenberg <bryan.wieschenberg@gmail.com>
2025-07-16 15:56:56 -0400

    LC 7/16/25

--- leetcode/medium.py
+++ leetcode/medium.py
@@ -1,3 +1,4 @@
+from utils import p, e
 from typing import List
 
 class Solution:
@@ -19,7 +20,38 @@ class Solution:
                 j += 1 # Trainer too weak, try next trainer
                 
         return matches
+    # https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i/description
+    # Time : O(n)
+    # Space: O(1)
+    def maximumLength(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
 
+        even_count = sum(1 for num in nums if num % 2 == 0)
+        odd_count = len(nums) - even_count
+
+        same_parity_max = max(even_count, odd_count)
+
+        alt_even_first = 0
+        expecting_even = True
+        for num in nums:
+            if (num % 2 == 0) == expecting_even:
+                alt_even_first += 1
+                expecting_even = not expecting_even
+
+        alt_odd_first = 0
+        expecting_odd = True
+        for num in nums:
+            if (num % 2 == 1) == expecting_odd:
+                alt_odd_first += 1
+                expecting_odd = not expecting_odd
+
+        alternating_max = max(alt_even_first, alt_odd_first)
+
+        return max(same_parity_max, alternating_max)
+    
 s = Solution()
 
-print(s.matchPlayersAndTrainers( [4,7,9], [8,2,5,8] ))
+# print(s.matchPlayersAndTrainers( [4,7,9], [8,2,5,8] ))
+
+print(s.maximumLength( [1,2,1,1,2,1,2] ))
