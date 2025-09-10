eb5f301b8bc22d485451e4fd8201db7fc83adf6a
BryanWieschenberg <bryan.wieschenberg@gmail.com>
2025-09-10 17:27:11 -0400

    NC150 2 pointers complete

---
+++ lib_essentials.py
@@ -0,0 +1,131 @@
+from collections import Counter, defaultdict
+import itertools
+import math
+
+
+# ---------------- ITERATION ----------------
+
+# enumerate - adds a counter to an iterable and returns it as an enumerate object
+# Time: O(n), Space: O(1)
+nums = [10, 20, 30]
+for i, val in enumerate(nums):
+    print(i, val)  # 0 10 | 1 20 | 2 30
+
+# zip
+# Time: O(min(n, m)), Space: O(1)
+a, b = [1, 2, 3], ['x', 'y', 'z']
+for x, y in zip(a, b):
+    print(x, y)  # 1 x | 2 y | 3 z
+
+# reversed
+# Time: O(n) iteration, Space: O(1)
+print(list(reversed(a)))  # [3, 2, 1]
+
+# any / all
+# Time: O(n) worst, Space: O(1)
+print(any([0, 0, 1]))  # True
+print(all([1, 2, 3]))  # True
+
+
+# ---------------- LISTS ----------------
+
+# sort
+# Time: O(n log n), Space: O(n)
+arr = [3, 1, 2]
+arr.sort()
+print(arr)  # [1, 2, 3]
+
+# slicing
+# Time: O(k), Space: O(k)
+print(arr[::-1])  # [3, 2, 1]
+
+# sum, max, min
+# Time: O(n), Space: O(1)
+print(sum(arr), max(arr), min(arr))  # 6 3 1
+
+
+# ---------------- DICTIONARIES ----------------
+
+# get
+# Time: O(1) average, Space: O(1)
+d = {"a": 1, "b": 2}
+print(d.get("c", 0))  # 0
+
+# items
+# Time: O(n) iterate, Space: O(1)
+for k, v in d.items():
+    print(k, v)  # a 1 | b 2
+
+# Counter
+# Time: O(n), Space: O(k) where k = unique items
+cnt = Counter("leetcode")
+print(cnt)  # Counter({'e': 3, 'l': 1, 't': 1, 'c': 1, 'o': 1, 'd': 1})
+
+# defaultdict
+# Time: O(1) avg access, Space: O(n)
+dd = defaultdict(list)
+dd["x"].append(1)
+print(dd)  # defaultdict(<class 'list'>, {'x': [1]})
+
+
+# ---------------- SETS ----------------
+
+s1, s2 = {1, 2, 3}, {2, 3, 4}
+print(s1 & s2)  # {2, 3}  (intersection)
+print(s1 | s2)  # {1, 2, 3, 4} (union)
+print(s1 - s2)  # {1} (difference)
+
+
+# ---------------- MATH ----------------
+
+# divmod
+# Time: O(1), Space: O(1)
+print(divmod(7, 3))  # (2, 1)
+
+# pow with mod
+# Time: O(log y), Space: O(1)
+print(pow(2, 10, 1000))  # 24
+
+# gcd/lcm
+# Time: O(log(min(a, b))), Space: O(1)
+print(math.gcd(12, 18), math.lcm(12, 18))  # 6 36
+
+
+# ---------------- ITERTOOLS ----------------
+
+# permutations
+# Time: O(nPr), Space: O(r)
+print(list(itertools.permutations([1, 2], 2)))  # [(1, 2), (2, 1)]
+
+# combinations
+# Time: O(nCr), Space: O(r)
+print(list(itertools.combinations([1, 2, 3], 2)))  # [(1, 2), (1, 3), (2, 3)]
+
+# product
+# Time: O(len(a)*len(b)), Space: O(1)
+print(list(itertools.product([1, 2], ['a', 'b'])))
+# [(1, 'a'), (1, 'b'), (2, 'a'), (2, 'b')]
+
+
+# ---------------- STRINGS ----------------
+
+# ord / chr
+# Time: O(1), Space: O(1)
+print(ord('a'), chr(97))  # 97 a
+
+# split / join
+# Time: O(n), Space: O(n)
+s = "a b c".split()
+print(s)  # ['a', 'b', 'c']
+print("".join(s))  # abc
+
+# isalpha
+# isdigit - 
+# isdigit - 
+# Time: O(n), Space: O(1)
+print("abc".isalpha(), "123".isdigit(), "abc123".isalnum())  # True True True
+print("abc!".isalpha(), "abc!".isdigit(), "abc 123".isalnum())  # False False False
+
+# reverse string
+# Time: O(n), Space: O(n)
+print("hello"[::-1])  # olleh
---
+++ neetcode150/Two_Pointers.py
@@ -0,0 +1,106 @@
+from typing import List
+import time
+
+class Solution:
+    # https://neetcode.io/problems/valid-palindrome?list=neetcode150
+    # Time: O(n)
+    # Space: O(1)
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+
+        while l < len(s) / 2:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+    
+    # https://neetcode.io/problems/two-sum-ii-input-array-is-sorted?list=neetcode150
+    # Time: O(n)
+    # Space: O(1)
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l != r:
+            total = numbers[l] + numbers[r]
+            if total == target:
+                return [l+1, r+1]
+            elif total < target:
+                l += 1
+            else:
+                r -= 1
+        return None
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            l, r = i + 1, len(nums)-1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+        return res
+    
+    # https://neetcode.io/problems/container-with-most-water?list=neetcode150
+    # Time: O(n)
+    # Space: O(1)
+    def maxArea(self, heights: List[int]) -> int:
+        l, r, res = 0, len(heights)-1, 0
+        while l < r:
+            area = (r - l) * min(heights[l], heights[r])
+            res = max(area, res)
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+    
+    # https://neetcode.io/problems/trapping-rain-water?list=neetcode150
+    # Time: O(n)
+    # Space: O(1)
+    def trap(self, heights: List[int]) -> int:
+        l, r, res = 0, len(heights)-1, 0
+        maxL, maxR = heights[l], heights[r]
+        while l < r:
+            if heights[l] < heights[r]:
+                l += 1
+                maxL = max(maxL, heights[l])
+                res += maxL - heights[l]
+            else:
+                r -= 1
+                maxR = max(maxR, heights[r])
+                res += maxR - heights[r]
+        return res
+    
+s = Solution()
+
+# print(s. isPalindrome ( "Was it a car or a cat I saw?" )) # True
+
+# print(s. twoSum ( numbers=[1,2,5,7,9], target=12 )) # [1,2]
+
+# print(s. threeSum ( nums=[-1,0,1,2,-1,-4] )) # [[-1,-1,2],[-1,0,1]]
+
+# print(s. maxArea ( heights=[1,7,2,5,4,7,3,6] )) # 49
+
+# print(s. trap ( heights=[0,2,0,3,1,0,1,3,2,1] )) # 9
