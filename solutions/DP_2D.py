daebd42ed1123a6fb2934bd6b0f6d2bf7aba3608
BryanWieschenberg <bryan.wieschenberg@gmail.com>
2026-02-17 21:16:35 -0500

    NC150: DP(2d)

--- solutions/DP_2D.py
+++ solutions/DP_2D.py
@@ -1,5 +1,6 @@
 from sys import path; from os import path as ospath; path.append(ospath.dirname(ospath.dirname(__file__)))
 from typing import List
+from functools import lru_cache
 
 class Solution:
     # 191
@@ -71,8 +72,21 @@ class Solution:
     
     # 199
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        pass
-
+        n1, n2, n3 = len(s1), len(s2), len(s3)
+        if n1 + n2 != n3:
+            return False
+        dp = [[False] * (n2+1) for _ in range(n1+1)]
+        dp[n1][n2] = True
+        for i in range(n1, -1, -1):
+            for j in range(n2, -1, -1):
+                if i == n1 and j == n2:
+                    continue
+                dp[i][j] = (
+                    (i < n1 and s1[i] == s3[i + j] and dp[i + 1][j]) or
+                    (j < n2 and s2[j] == s3[i + j] and dp[i][j + 1])
+                )
+        return dp[0][0]
+    
     # 200
     def stoneGame(self, piles: List[int]) -> bool:
         pass
@@ -83,23 +97,89 @@ class Solution:
 
     # 202
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        pass
+        @lru_cache(None)
+        def dfs(r, c):
+            res = 1
+            for dr, dc in dirs:
+                nr, nc = r+dr, c+dc
+                if 0 <= nr < R and 0 <= nc < C and matrix[nr][nc] > matrix[r][c]:
+                    res = max(res, 1 + dfs(nr, nc))
+            return res
+        
+        R, C = len(matrix), len(matrix[0])
+        dirs = ((0,1),(1,0),(0,-1),(-1,0))
+        res = 0
+        for r in range(R):
+            for c in range(C):
+                res = max(res, dfs(r, c))
+        return res
 
     # 203
     def numDistinct(self, s: str, t: str) -> int:
-        pass
+        @lru_cache(None)
+        def dfs(i, j):
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+            if s[i] == t[j]:
+                return dfs(i+1, j+1) + dfs(i+1, j)
+            else:
+                return dfs(i+1, j)
+        
+        return dfs(0, 0)
 
     # 204
     def minDistance(self, word1: str, word2: str) -> int:
-        pass
-
+        R, C = len(word1), len(word2)
+        dp = list(range(C+1))
+        for i in range(1, R+1):
+            curr = [i] + [0] * C
+            for j in range(1, C+1):
+                if word1[i-1] == word2[j-1]:
+                    curr[j] = dp[j-1]
+                else:
+                    curr[j] = 1 + min(
+                        curr[j-1],
+                        dp[j],
+                        dp[j-1]
+                    )
+            dp = curr
+        return dp[C]
+    
     # 205
     def maxCoins(self, nums: List[int]) -> int:
-        pass
-
+        @lru_cache(None)
+        def dfs(l, r):
+            if l > r:
+                return 0
+            res = 0
+            for i in range(l, r+1):
+                coins = nums[l-1] * nums[i] * nums[r+1]
+                coins += dfs(l, i-1) + dfs(i+1, r)
+                res = max(res, coins)
+            return res
+            
+        nums = [1] + nums + [1]
+        return dfs(1, len(nums)-2)
+    
     # 206
     def isMatch(self, s: str, p: str) -> bool:
-        pass
+        @lru_cache(None)
+        def dfs(i, j):
+            if j == len(p):
+                return i == len(s)
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+            if j+1 < len(p) and p[j+1] == '*':
+                return (
+                    dfs(i, j+2) or          # don't use *
+                    (match and dfs(i+1, j)) # use *
+                )
+            if match:
+                return dfs(i+1, j+1)
+            return False
+        
+        return dfs(0, 0)
 
 if __name__ == "__main__":
     s = Solution()
